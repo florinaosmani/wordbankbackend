6bd6bcb6a488f76f1567dd07c90b42632b96cf65
Florina Osmani <florina.osmani@hotmail.com>
2026-07-05 23:03:52 +0200

    04A - CRUD funktionalität

--- wordbankbackend/src/main/java/ch/fo/wordbankbackend/controller/WordController.java
+++ wordbankbackend/src/main/java/ch/fo/wordbankbackend/controller/WordController.java
@@ -3,9 +3,8 @@ package ch.fo.wordbankbackend.controller;
 import ch.fo.wordbankbackend.model.Definition;
 import ch.fo.wordbankbackend.model.Word;
 import ch.fo.wordbankbackend.service.WordService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,13 +17,54 @@ public class WordController {
         this.wordService = wordService;
     }
 
-    @GetMapping("api/words")
+    /**
+     * Gibt alle Wörter zurück.
+     * @return Alle Wörter.
+     */
+    @GetMapping("/api/words")
     public List<Word> getWords() {
         return wordService.getAllWords();
     }
 
-    @GetMapping("api/words/{id}")
+    /**
+     * Gibt ein Wort mit der gewünschten ID zurück.
+     * @param id ID des gewünschten Wortes.
+     * @return Wort mit der gewünschten ID.
+     */
+    @GetMapping("/api/words/{id}")
     public Word getWordById(@PathVariable String id){
         return wordService.getWordById(id);
+    }
+
+    /**
+     * Erstellt ein neues Wort.
+     * @param word Das Wort, welches man erstellen möchte.
+     * @return Das neu erstellte Word.
+     */
+    @PostMapping("/api/words")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Word createWord(@RequestBody Word word){
+        return wordService.createWord(word);
+    }
+
+    /**
+     * Das gewünschte Wort aktualisieren.
+     * @param id ID des zu aktualisierenden Wortes.
+     * @param word Die aktualisierte Version des Wortes.
+     * @return Das aktualisierte Wort.
+     */
+    @PutMapping("/api/words/{id}")
+    public Word updateWord(@PathVariable String id, @RequestBody Word word){
+        return wordService.updateWord(id, word);
+    }
+
+    /**
+     * Löscht das gewünschte Wort.
+     * @param id ID des zu löschenden Wortes.
+     */
+    @DeleteMapping("/api/words/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteWord(@PathVariable String id){
+        wordService.deleteWord(id);
     }
 }
--- wordbankbackend/src/main/java/ch/fo/wordbankbackend/service/WordService.java
+++ wordbankbackend/src/main/java/ch/fo/wordbankbackend/service/WordService.java
@@ -18,7 +18,7 @@ public class WordService {
 
     /**
      * Liefert alle Wörter aus der DB, samt deren Definitionen.
-     * @return Liste mit allen Wörtern
+     * @return Liste mit allen Wörtern.
      */
     public List<Word> getAllWords() {
         return wordRepository.findAll();
@@ -26,10 +26,38 @@ public class WordService {
 
     /**
      * Sucht ein Wort anhand seiner ID und gibt es zurück.
-     * @param id Die Id des gesuchten Wortes
-     * @return Das Wort mit der passenden ID, oder null, wenn nicht gefunden
+     * @param id Die Id des gesuchten Wortes.
+     * @return Das Wort mit der passenden ID, oder null, wenn nicht gefunden.
      */
     public Word getWordById(String id) {
         return wordRepository.findById(id).orElse(null);
+    }
+
+    /**
+     * Speichert ein neues Wort in der DB.
+     * @param word Das zu speichernde Wort.
+     * @return Das in der DB gespeicherte Wort.
+     */
+    public Word createWord(Word word) {
+        return wordRepository.save(word);
+    }
+
+    /**
+     * Aktualisiert ein bestehendes Wort.
+     * @param id Die ID des zu verändernde Wort.
+     * @param word Daso Wort mit den Änderungen.
+     * @return Das in der DB aktualisierte Wort.
+     */
+    public Word updateWord(String id, Word word){
+        word.setId(id);
+        return wordRepository.save(word);
+    }
+
+    /**
+     * Löscht ein Wort aus der DB.
+     * @param id Die ID des zu löschenden Worts.
+     */
+    public void deleteWord(String id){
+        wordRepository.deleteById(id);
     }
 }
